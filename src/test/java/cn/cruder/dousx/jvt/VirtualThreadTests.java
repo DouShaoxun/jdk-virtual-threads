5365f11fae9dcff008e22c416fc1abe0f083a650
dousx <dousx_cn@163.com>
2023-09-19 22:37:37 +0800

    初探Jdk虚拟线程

---
+++ src/test/java/cn/cruder/dousx/jvt/VirtualThreadTests.java
@@ -0,0 +1,24 @@
+package cn.cruder.dousx.jvt;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class VirtualThreadTests {
+    @Test
+    void ofVirtualTest() {
+        for (int i = 0; i < 10; i++) {
+            final var k = i;
+            Thread.ofVirtual().name(String.format("vt-%s", k))
+                    .start(() -> {
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(3);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        System.out.println(Thread.currentThread().getName() + " " + k);
+                    });
+        }
+
+    }
+}
